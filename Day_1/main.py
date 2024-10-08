cbca266da43a687aa9922dcc55bf824922ed4252
DGbolaga <gbolagadaramola765@gmail.com>
2024-10-08 08:18:28 +0100

    Band Name Generator - Day_1

---
+++ Day_1/main.py
@@ -0,0 +1,12 @@
+########## Topic: Working with Variables in Python to manage data ###############
+
+# Day 1 Project: Band Name Generator. 
+
+print("Welcome the band name Generator.")
+
+nameOfUserCity = input("What is the name of the city you grew up in? \n")
+
+nameOfUserPet = input("What is the name of your pet? \n")
+
+print("Your band name could be " + nameOfUserCity + " " + nameOfUserPet )
+
