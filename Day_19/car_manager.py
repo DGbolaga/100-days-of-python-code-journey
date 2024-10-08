5588d5391791f93a29d181053a49c6dfc03fbc43
DGbolaga <gbolagadaramola765@gmail.com>
2024-10-08 10:57:19 +0100

    Update car_manager.py

--- Day_19/car_manager.py
+++ Day_19/car_manager.py
@@ -6,17 +6,6 @@ STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-# Create cars that are 20px high by 40px wide that are randomly generated along the y-axis and move to the left edge
-# of the screen. No cars should be generated in the top and bottom 50px of the screen (think of it as a safe zone for
-# our little turtle). Hint: generate a new car only every 6th time the game loop runs. If you get stuck,
-# check the video walkthrough in Step 4.
-
-# Detect when the turtle player has reached the top edge of the screen (i.e., reached the FINISH_LINE_Y). When this
-# happens, return the turtle to the starting position and increase the speed of the cars. Hint: think about creating
-# an attribute and using the MOVE_INCREMENT to increase the car speed. If you get stuck, check the video walkthrough
-# in Step 6.
-
-
 class CarManager(Turtle):
 
     def __init__(self):
