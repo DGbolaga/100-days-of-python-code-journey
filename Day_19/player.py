27510d7ed2ba1db431dca60ff5ec8fa21a5cc822
DGbolaga <gbolagadaramola765@gmail.com>
2024-10-08 10:58:16 +0100

    Update player.py

--- Day_19/player.py
+++ Day_19/player.py
@@ -3,13 +3,6 @@ from turtle import Turtle
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
-# Create a turtle player that starts at the bottom of the screen and listen for the "Up" keypress to move the turtle
-# north.
-
-# Detect when the turtle player has reached the top edge of the screen (i.e., reached the FINISH_LINE_Y). When this
-# happens, return the turtle to the starting position and increase the speed of the cars. Hint: think about creating
-# an attribute and using the MOVE_INCREMENT to increase the car speed. If you get stuck, check the video walkthrough
-# in Step 6.
 
 
 class Player(Turtle):
